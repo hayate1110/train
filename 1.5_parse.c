623ad37edef2797b9f1b0ed0782cfa5e1fd9b276
Hayate Shiroma <e225719@ie.u-ryukyu.ac.jp>
2026-08-25 11:34:41 +0900

    1.5 列車データ行の解析

---
+++ 1.5_parse.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/* データセットを意識して読み込む */
+
+int main(void)
+{
+    int count;
+    char buf[64];
+
+    while (1) {
+        /* データセットの先頭（データ数）を読む */
+        if (fgets(buf, sizeof(buf), stdin) == NULL) {
+            break; /* データセットの異常終了 */
+        }
+        sscanf(buf, "%d", &count); /* データセット中のデータ数 */
+        printf("%d\n", count); /* データ数の書き出し */
+
+        if (count == 0) { /*.データ全体の終了の判定 */
+            break;
+        }
+
+        /* データセット（列車データ）を読み込む */
+        while (count-- > 0) {
+            if (fgets(buf, sizeof(buf), stdin) == NULL) {
+                break; /* データセットの異常終了 */
+            }
+
+            /* printf("%s", buf); */ /* 列車データの書き出し */
+        }
+    }
+}
+
+/* 列車データの解析 */
+int parse_connection(char *buf)
+{
+    char from[18], to[18];
+    int dpt[2], arv[2], fare;
+    
+    sscanf(buf, "%s %d:%d &s %d:%d %d", 
+        from, &(dpt[0]), &(dpt[1]), to, &(arv[0]), (&arv[1]), &fare);
+
+    printf("%s %d %s %d %d\n", 
+        from, dpt[0]*60+dpt[1], to, arv[0]*60+arv[1], fare);
+}
