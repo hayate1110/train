c6481b6dae44c517c0a407c7570b51c80ebf79de
Hayate Shiroma <e225719@ie.u-ryukyu.ac.jp>
2026-08-25 11:17:55 +0900

    1.3 データ数の行と列車データの行を判別する

---
+++ 1.3_identify_data_rows.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* データ数の行と列車データの行を判別する */
+
+int main(void)
+{
+    int count;
+    char buf[64];
+
+    count = 0;
+    while (fgets(buf, sizeof(buf), stdin) != NULL) {
+        if (count == 0) { /* データ数の行 */
+            sscanf(buf, "%i", &count);
+            printf("%i\n", count);
+        }
+        else { /* 列車データの行 */
+            /* fputs(buf, stdout); */
+            count--;
+        }
+    }
+}
