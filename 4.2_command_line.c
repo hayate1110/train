1604434a53052d1cf415520ddb322e363f2d6edc
Hayate Shiroma <e225719@ie.u-ryukyu.ac.jp>
2026-09-07 17:24:31 +0900

    4.2 チェックポイント

---
+++ 4.2_command_line.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    char *st1 = NULL;
+    char *st2 = NULL;
+    int dpt_h, dpt_m;
+    int arv_h, arv_m;
+    int meet;
+
+    for (int i = 1; i < argc; i++) {
+
+        if (strcmp(argv[i], "-st1") == 0) {
+            st1 = argv[++i];
+
+        } else if (strcmp(argv[i], "-st2") == 0) {
+            st2 = argv[++i];
+
+        } else if (strcmp(argv[i], "-dpt") == 0) {
+            sscanf(argv[++i], "%d:%d", &dpt_h, &dpt_m);
+
+        } else if (strcmp(argv[i], "-arv") == 0) {
+            sscanf(argv[++i], "%d:%d", &arv_h, &arv_m);
+
+        } else if (strcmp(argv[i], "-m") == 0) {
+            meet = atoi(argv[++i]);
+
+        } else {
+            fprintf(stderr, "usage: %s -st1 <string> -st2 <string> -dpt <int>:<int> -arv <int>:<int> -m <int>\n", argv[0]);
+            return 1;
+        }
+    }
+
+    printf("st1 = %s\n", st1);
+    printf("st2 = %s\n", st2);
+    printf("dpt = %02d:%02d\n", dpt_h, dpt_m);
+    printf("arv = %02d:%02d\n", arv_h, arv_m);
+    printf("m = %d\n", meet);
+
+    return 0;
+}
